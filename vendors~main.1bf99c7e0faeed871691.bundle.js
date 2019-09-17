ac13cfe40245358861239b5e022fc8e36975276f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-17 12:45:33 +0000

    Deploy mkosir/react-magnetic-board to github.com/mkosir/react-magnetic-board.git:gh-pages

--- vendors~main.1bf99c7e0faeed871691.bundle.js
+++ vendors~main.1bf99c7e0faeed871691.bundle.js
@@ -82,4 +82,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.8adef48bd2b0358f020a.bundle.js.map
+//# sourceMappingURL=vendors~main.1bf99c7e0faeed871691.bundle.js.map
